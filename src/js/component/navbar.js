bc7fd4f6399abae539ddb7d2501f72c14f9fee3c
orosell666 <o.rosell.ribas@gmail.com>
2022-03-01 23:30:27 +0000

    falta muchoou

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
+		<nav className="navbar navbar-black bg-black mb-3">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<span className="navbar-brand mb-0 h1"><img className="img-fluid ms-5" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/Star_wars_logo_alternate.svg/150px-Star_wars_logo_alternate.svg.png" alt="Logo" /></span>
 			</Link>
 			<div className="ml-auto">
 				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
+					<button className="btn btn-warning me-5">Favorites</button>
 				</Link>
 			</div>
 		</nav>
--- src/js/layout.js
+++ src/js/layout.js
@@ -5,6 +5,7 @@ import ScrollToTop from "./component/scrollToTop";
 import { Home } from "./views/home";
 import { Demo } from "./views/demo";
 import { Single } from "./views/single";
+// import { Characters } from "./views/characters"
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
@@ -28,8 +29,8 @@ const Layout = () => {
 						<Route exact path="/demo">
 							<Demo />
 						</Route>
-						<Route exact path="/single/:theid">
-							<Single />
+						<Route exact path="/characters">
+							<Characters />
 						</Route>
 						<Route>
 							<h1>Not found!</h1>
