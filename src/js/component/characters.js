be89a904c4cfd45ff591f2359b4146ac30ad3b25
orosell666 <o.rosell.ribas@gmail.com>
2022-03-18 17:33:07 +0000

    pinta, mas bootstrap

--- src/js/component/characters.js
+++ src/js/component/characters.js
@@ -1,9 +1,10 @@
 import React, { useContext, useEffect  } from "react";
 import { Context } from "../store/appContext";
+import propTypes from "prop-types";
 
 import "../../styles/home.css";
 
-export const Characters = () => {
+export const Characters = (props) => {
   const { actions, store } = useContext(Context);
 
   useEffect(() => {
@@ -16,7 +17,7 @@ export const Characters = () => {
     <div className="card col-3 mx-2" >
       <img src="https://starwars-visualguide.com/#/characters/1" className="card-img-top" alt="..."  />
       <div className="card-body">
-        <h5 className="card-title">Character</h5>
+        <h5 className="card-title">{props.character.name}</h5>
         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
         <button type="button" className="btn btn-warning" data-bs-toggle="modal" data-bs-target="#exampleModal">
   More Info...
@@ -49,6 +50,7 @@ export const Characters = () => {
     </div>
   </div>
 	);
+  
  };
 
- 
+ Characters.propTypes = {character:propTypes.json}
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -3,20 +3,28 @@ import { Context } from "../store/appContext";
 import { Characters } from "../component/characters";
 import { Planets } from "../component/planets";
 import { Starships } from "../component/starships";
+
 import "../../styles/home.css";
 
 
 
 export const Home = () => {
-	
+	const { actions, store } = useContext(Context);
 
 	return(
 	
 	
 	<div className="container col-8">
-		<div>
+		<div >
 			<h1 className="sectionName">Characters</h1>
-			<Characters/>			
+			{store.character.map((value,index) =>{ console.log(value)
+				return (
+					<div >
+					<Characters key={index}character={value}/>
+					</div>
+				)
+			})}
+						
 		</div>
 		<div>
 			<h1 className="sectionName">Planets</h1>
