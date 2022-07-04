bc6c6346f23743dc3e2cbcccebcf6adcb62c40b3
orosell666 <o.rosell.ribas@gmail.com>
2022-07-04 13:37:28 +0000

    arreglos front

--- src/js/component/characters.js
+++ src/js/component/characters.js
@@ -26,14 +26,14 @@ export const Characters = (props) => {
   More Info...
 </button></div>
       <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div className="modal-dialog">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h5 className="modal-title" id="exampleModalLabel">person</h5>
+        <div className="card modal-dialog">
+          <div className="card modal-content">
+            <div className="card modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">{props.character.name}</h5>
               <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <img src="https://starwars-visualguide.com/#/characters/1" className="card-img-top" alt="..."  />
-            <div className="modal-body">
+            <div className="card modal-body">
           <p>Height: {props.character.height}</p>
           <p>Weight:{props.character.mass}</p>
           <p>Hair color:{props.character.hair_color}</p>
